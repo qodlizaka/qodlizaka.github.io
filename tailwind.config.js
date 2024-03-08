86587e8d4948e548d8ea0c799bba37e45531e01c
707QodliZaka <qodlizaka513@gmail.com>
2024-03-08 21:06:56 +0700

    Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    // "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{html}",
   ],
   theme: {
     colors: {
